e40386aacbb4f903cbed92615d02a5d7dd1e5cda
Joshua C <thejosh223@gmail.com>
2015-01-24 12:22:24 +0800

    Game Manager references fixed.

--- Assets/Scripts/Util/SceneLoader.cs
+++ Assets/Scripts/Util/SceneLoader.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class SceneLoader : ExtBehaviour {
-	void Start() {
+	void Awake() {
 		Application.LoadLevelAdditive("MainScene");
 	}
 
-	void Update() {
+	void Start() {
 		//load stuff into GameMgr game object
 		GameObject goRoot = GameObject.Find("Root");
 		if (goRoot != null) {
@@ -14,14 +14,13 @@ public class SceneLoader : ExtBehaviour {
 			while (goRoot.transform.childCount > 0) {
 				goRoot.transform.GetChild(0).parent = gameObject.transform;
 			}
-
+			
 			Destroy(goRoot);
-
+			
 			GetNotifyMgr().PostNotify(NotifyType.LoadGameSceneDone, this);
-
+			
 			//once loading is done, destroy this script
 			Destroy(this);
 		}
-
 	}
 }
