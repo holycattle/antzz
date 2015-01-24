b250c3d4fc064facf1b546f8bbe63623d1c92368
Joniel Ibasco <jlibasco@gmail.com>
2015-01-25 06:38:56 +0800

    followAnt bool

--- Assets/Scripts/Components/AntController.cs
+++ Assets/Scripts/Components/AntController.cs
@@ -6,6 +6,8 @@ public class AntController : ExtBehaviour {
 
 	private static AntController[] antList;
 
+    public bool followAnt;
+
 	public float moveSpeed = 1f; // units per second
 	public float rotateSpeed = 360f; // degrees per second
 	public float scanRadius = 1f;
@@ -53,12 +55,16 @@ public class AntController : ExtBehaviour {
 	IEnumerator UpdateScan() {
 		isScanning = true;
 		float offsetAngle = Scan();
-		if (Mathf.Abs(offsetAngle) < followAngleThreshold) {
-			float newOffset = ScanAnts();
-			if (newOffset != 0) {
-				offsetAngle = newOffset;
-			}
-		}
+
+        if (followAnt) {
+            if (Mathf.Abs(offsetAngle) < followAngleThreshold) {
+                float newOffset = ScanAnts();
+                if (newOffset != 0) {
+                    offsetAngle = newOffset;
+                }
+            }
+        }
+        
 		curOffsetAngle = offsetAngle;
 
 		yield return null;
